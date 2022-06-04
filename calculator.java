5c00434b63367d6458c6b8aed5ef8859e904a2d1
adinath somwanshi <somwanshiadi97@gmail.com>
2022-06-04 20:21:42 +0530

    Second change

---
+++ calculator.java
@@ -0,0 +1,29 @@
+
+public class calculate
+{
+    int num1 , num2 , result;
+
+    public void add()
+    {
+        num1 = 20;
+
+        num2 = 50;
+
+        result = num1 + num2;
+
+        System.out.println("Addition = " + result);
+    }
+
+    public void sub()
+    {
+        num1 = 60;
+
+        num2 = 40;
+
+        result = num1 - num2;
+
+        System.out.println("Subtraction = " + result);
+
+    }
+}
+
